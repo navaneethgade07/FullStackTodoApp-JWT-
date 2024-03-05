106153591763ff25b57360451852149f6fe30ddd
Navaneeth Gade <117298644+navaneethgade07@users.noreply.github.com>
2024-03-05 22:59:25 +0530

    improve

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./db/connect');
 const userRoutes = require('./routes/user'); 
 const todoRoutes = require('./routes/todo');
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 require('dotenv').config();
 
 const app = express();
@@ -14,40 +14,36 @@ app.use(cors());
 
 app.use(cookieParser());
 
-
-app.set('view engine', 'ejs');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.set('view engine', 'ejs');
 
-
-app.use('/',userRoutes);
-app.use('/',todoRoutes);
-
-//  create a session and store data in a cookie in client side
 app.use(cookieSession({
-    name: 'session',
-    keys: process.env.SECRET,
-    httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000 ,
+  name: 'session',
+  keys: [process.env.SECRET],
+  httpOnly: true,
+  maxAge: 24 * 60 * 60 * 1000,
 }));
 
-app.get('/',(req,res)=>{
-    res.render('landingpage');
+app.use('/', userRoutes);
+app.use('/', todoRoutes);
+
+app.get('/', (req, res) => {
+  res.render('landingpage');
 });
 
-const start = async()=>{
-    try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port,()=>{
-            console.log(`server is running on http://localhost:${port}`);
-        });
-    } catch (error) {
-        console.log(error);
-    }
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 start();
