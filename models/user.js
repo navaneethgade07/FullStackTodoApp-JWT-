9e6c3b81c87c7626042745699a5ee71829d0784b
Navaneeth Gade <117298644+navaneethgade07@users.noreply.github.com>
2024-03-05 23:57:51 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,33 +4,19 @@ const createSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
   },
+  
   username: {
     type: String,
     required: true,
     unique: true,
-    trim: true,
   },
+  
   password: {
     type: String,
     required: true,
     minlength: 6,
   },
 });
-
-// Virtual method to compute the user's full name
-createSchema.virtual('fullName').get(function () {
-  return `${this.firstName} ${this.lastName}`;
-});
-
-// Method to exclude the password field when the document is converted to JSON
-createSchema.methods.toJSON = function () {
-  const obj = this.toObject();
-  delete obj.password;
-  return obj;
-};
 
 module.exports = mongoose.model('newUser', createSchema);
