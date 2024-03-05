b8e2a21540395b030feccbfdcab0eb4c14990b28
Navaneeth Gade <117298644+navaneethgade07@users.noreply.github.com>
2024-03-05 23:51:43 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -2,14 +2,6 @@ const mongoose = require('mongoose');
 
 const connectDB = (url) => {
   return mongoose.connect(url)
-    .then(() => {
-      console.log('Connected to the database');
-      return mongoose.connection;
-    })
-    .catch((error) => {
-      console.error('Error connecting to the database:', error);
-      process.exit(1);
-    });
 };
 
 module.exports = connectDB;
