d14821334cba9462eaf9501aedde19db64bc8dd8
Navaneeth Gade <117298644+navaneethgade07@users.noreply.github.com>
2024-03-05 22:59:23 +0530

    improve

--- models/todo.js
+++ models/todo.js
@@ -1,15 +1,19 @@
 const mongoose  = require('mongoose');
 
+const User = require('./userModel'); // assuming the User model is in a separate file named userModel.js
+
 const todoSchema = new mongoose.Schema({
     task: { 
         type: String,
         required: true 
     },
-    completed : Boolean,
-
-    userID: {
+    completed : {
+        type: Boolean,
+        default: false
+    },
+    user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'newUser'
+        ref: 'User'
     }
 });
 
